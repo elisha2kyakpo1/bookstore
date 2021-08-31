c92150a4fa369b6a4f2717318dcdb0ac9ccfd116
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-08-31 18:26:01 +0200

    Add style for bookstore

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -5,14 +5,20 @@ const Books = () => {
     {
       title: 'The Hunger Game',
       author: 'suzane collins',
+      percentComplete: '64%',
+      chapter: 'Chapter 17',
     },
     {
       title: 'Dune',
       author: 'Frank Herbert',
+      percentComplete: '8%',
+      chapter: 'Chapter 3: "A lesson learned"',
     },
     {
       title: 'Capital in the Twenty-First Century',
       author: 'suzane collins',
+      percentComplete: '0%',
+      chapter: 'Introduction',
     },
   ]);
 
@@ -39,23 +45,41 @@ const Books = () => {
                 className="books"
                 key={book.title}
               >
-                <h4>Action</h4>
-                <div>
-                  <h2>{book.title}</h2>
-                  <h4>{book.author}</h4>
+                <div className="title-section">
+                  <div>
+                    <h4>Action</h4>
+                    <h2>{book.title}</h2>
+                    <h4>{book.author}</h4>
+                  </div>
+                  <div className="action-buttons">
+                    <span type="button">Comments</span>
+                    <span type="button">Remove</span>
+                    <span type="button">Edit</span>
+                  </div>
                 </div>
-                <div>
+                <div className="percent">
                   <div className="circle"> </div>
+                  <div>
+                    <h3>{book.percentComplete}</h3>
+                    <p>Completed</p>
+                  </div>
+                </div>
+
+                <div>
+                  <h4>Current Chapter</h4>
+                  <h2>{book.chapter}</h2>
+                  <button className="update-progress" type="button">UPDATE PROGRESS</button>
                 </div>
               </div>
             ))
           }
         </div>
-        {/* <AddBook addBook={addBook} /> */}
       </div>
       <form className="form" onSubmit={HandleSub} action="#">
         <h1>ADD NEW BOOK</h1>
         <input className="input" type="text" onChange={(e) => setBook(e.target.value)} placeholder="Book title" />
+        <input className="input-cat" type="text" onChange={(e) => setBook(e.target.value)} placeholder="Book title" />
+        <input className="submit" type="submit" value="Add Book" />
       </form>
     </div>
   );
